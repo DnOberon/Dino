42e04af6f7cd4fb33f41e8bdc85f7df8d42723a8
dnoberon <johnw.darrington@gmail.com>
2019-05-06 21:37:04 -0600

    much cleaner - save on `map[string]interface{}` basic working

--- dino.go
+++ dino.go
@@ -1,3 +1,5 @@
+// Package dino provides an easy to use adapter to AWS's dynamodb. It aims to be flexible in both
+// manipulation and accessing of dynamodb data as well as promoting efficient data access patterns.
 package dino
 
 import (
@@ -12,6 +14,10 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
+// TODO set saner defaults for a package of this type
+var config aws.Config
+
+// Dino is the primary workhorse of the package. Contains the reference to the started DynamoDB session.
 type Dino struct {
 	session     *dynamodb.DynamoDB
 	primaryKeys []string
@@ -19,19 +25,34 @@ type Dino struct {
 	LastAction  Action
 }
 
+// Action contains information on the last executed action. Will also allow for replay/reversal of certain actions.
 type Action struct {
 	RawAction  interface{}
 	Input      interface{}
 	ExecutedAt time.Time
+	Snapshot   instanceState
 	Error      error
 }
 
-func NewDino(table *string, primaryKeys ...string) *Dino {
-	sess := session.Must(session.NewSession())
+type instanceState struct {
+	session     dynamodb.DynamoDB
+	primaryKeys []string
+	tableName   string
+	capturedAt  time.Time
+}
+
+// NewDino creates a new Dino instance with the provided table name and primary keys. This is the most basic
+// initialization of the service - more complicated configuration, including underlying access to the AWS Config
+// struct can be accomplished through the *Dino.Config() option after initialization.
+func NewDino(sess *session.Session, table *string, primaryKeys ...string) *Dino {
+	if sess == nil {
+		sess = session.Must(session.NewSession())
+	}
 
 	return &Dino{session: dynamodb.New(sess), primaryKeys: primaryKeys, tableName: table}
 }
 
+// Save persists data to Dino.tableName. Accepts either a struct (with proper tags) or a map[string]interface{}
 func (d *Dino) Save(input interface{}) Dino {
 	// first find out what type we're dealing with - supports map[string]interface, structs, []byte
 	if input == nil {
@@ -39,31 +60,38 @@ func (d *Dino) Save(input interface{}) Dino {
 		return *d
 	}
 
-	d.LastAction.Input = input
-	d.LastAction.ExecutedAt = time.Now().UTC()
-
-	inputValue := reflect.ValueOf(input)
-
-	switch inputValue.Kind() {
+	switch reflect.ValueOf(input).Kind() {
 	case reflect.Map:
-		d.saveMap()
-
-	case reflect.Struct:
+		d.saveMap(input)
 
 	default:
-		d.LastAction.Error = fmt.Errorf("unacceptable input type : %s", inputValue.Kind())
+		d.LastAction.Error = fmt.Errorf("unacceptable input type : %s", reflect.ValueOf(input).Kind())
 	}
 
 	return *d
 }
 
+// Error returns any errors from the last performed action
 func (d Dino) Error() error {
 	return d.LastAction.Error
 }
 
-func (d *Dino) saveMap() {
+func (d Dino) snapshot() instanceState {
+	return instanceState{
+		session:     *d.session,
+		primaryKeys: d.primaryKeys,
+		tableName:   *d.tableName,
+		capturedAt:  time.Now().UTC(),
+	}
+}
+
+func (d *Dino) saveMap(input interface{}) {
+	d.LastAction.Input = input
 	d.LastAction.RawAction = d.saveMap
-	inputMap := d.LastAction.Input.(map[string]interface{})
+	d.LastAction.ExecutedAt = time.Now().UTC()
+	d.LastAction.Snapshot = d.snapshot()
+
+	inputMap := input.(map[string]interface{})
 
 	// check for specified primaryKeys
 	for _, key := range d.primaryKeys {
@@ -75,30 +103,25 @@ func (d *Dino) saveMap() {
 		}
 	}
 
-	// TODO: Here we'll add the ability to flatten out slices and slices of struct
-	// that's a hard stop for it
-
 	// marshal and send
 	result, err := dynamodbattribute.MarshalMap(inputMap)
 	if err != nil {
+		// TODO issue #12 accept logger interface, default to stdout
 		log.Printf("%s", err.Error())
 		return
 	}
 
 	request := dynamodb.PutItemInput{
+		// ConditionExpression: TODO issue #13 should we accept condtion expressions
 		Item:      result,
 		TableName: d.tableName,
 	}
 
+	// TODO use item output in issue #11 - self consumed capacity management
 	_, err = d.session.PutItem(&request)
-
 	if err != nil {
 		d.LastAction.Error = err
 	}
 
 	return
-}
-
-func (d *Dino) saveStruct() {
-	d.LastAction.RawAction = d.saveStruct
 }
